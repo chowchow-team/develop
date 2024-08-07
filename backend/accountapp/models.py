870d9348812b99f2011b67f6295bb5e5921d56a4
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-08-07 11:28:24 +0900

    동물 계정 모델

--- backend/accountapp/models.py
+++ backend/accountapp/models.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta, timezone
 
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.core.validators import RegexValidator, EmailValidator
 from django.utils import timezone
@@ -21,7 +20,7 @@ class UserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None):
         if not username:
             raise ValueError('must have username')
-        user=self.model(
+        user = self.model(
             username=username,
             email=self.normalize_email(email),
         )
@@ -30,15 +29,15 @@ class UserManager(BaseUserManager):
         return user
     
     def create_superuser(self, username, email, password):
-        superuser=self.create_user(
+        superuser = self.create_user(
             username=username,
             email=email,
             password=password,
         )
-        superuser.is_admin=True
-        superuser.is_superuser=True
-        superuser.is_staff=True
-        superuser.is_active=True
+        superuser.is_admin = True
+        superuser.is_superuser = True
+        superuser.is_staff = True
+        superuser.is_active = True
         superuser.save()
         return superuser
     
@@ -53,6 +52,7 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_animal = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'username'
@@ -68,7 +68,11 @@ class User(AbstractBaseUser):
         is_new = self._state.adding
         super().save(*args, **kwargs)
         if is_new:
-            Profile.objects.create(user=self)
+            if self.is_animal:
+                AnimalUser.objects.create(user=self)
+                AnimalProfile.objects.create(user=self)
+            else:
+                Profile.objects.create(user=self)
 
     @classmethod
     def can_register(cls, username, email):
@@ -77,25 +81,33 @@ class User(AbstractBaseUser):
         if User.objects.filter(email=email, is_active=True).exists():
             return False, "이미 사용중인 이메일입니다."
         return True, ""
-    
 
-User = get_user_model()
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=13, blank=True, default='익명의 챠우')
-    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='default.png')
-    bio = models.TextField(blank=True, max_length=100)
+class AnimalUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    animal_num = models.IntegerField(default=0, unique=True)
+    center = models.CharField(max_length=13, blank=True, default='익명의 챠우')
+    species = models.CharField(max_length=100, blank=True)
+    breed = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.user.username
-    
+        return f"{self.user.username} - {self.species}"
+
+class BaseProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True, max_length=100)
+    nickname = models.CharField(max_length=13, blank=True, default='익명의 챠우')
+
+    class Meta:
+        abstract = True
+
     def save(self, *args, **kwargs):
         if self.pk:
-            old_profile = Profile.objects.get(pk=self.pk)
+            old_profile = self.__class__.objects.get(pk=self.pk)
             old_profile_pic = old_profile.profile_pic
             new_profile_pic = self.profile_pic
             if old_profile_pic.name != new_profile_pic.name:
-                if old_profile_pic.name != 'profile_pics/default.png':
+                if old_profile_pic.name != self._meta.get_field('profile_pic').default:
                     old_profile_pic.delete(save=False)
                 if new_profile_pic:
                     self.process_new_profile_pic()
@@ -103,7 +115,7 @@ class Profile(models.Model):
             if self.profile_pic:
                 self.process_new_profile_pic()
                 
-        super(Profile, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def process_new_profile_pic(self):
         image = Image.open(self.profile_pic)
@@ -114,3 +126,39 @@ class Profile(models.Model):
         image.save(output, format='JPEG', quality=90)
         output.seek(0)
         self.profile_pic.save(self.profile_pic.name, content=InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % os.path.splitext(self.profile_pic.name)[0], 'image/jpeg', sys.getsizeof(output), None), save=False)
+
+class Profile(BaseProfile):
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='default.png')
+    def __str__(self):
+        return self.user.username
+
+SPECIES_CHOICES = (
+    ('0','고양이'),
+    ('1','강아지'),
+    ('2','ETC'),
+)
+
+class AnimalProfile(BaseProfile):
+    '''
+    센터명: 마포센터 // 이름에서 추출. 설탕(마포센터-임시보호가능): '(' 뒤부터이름, '-'또는 ')' 앞까지 센터명 
+    종
+    품종
+    성별: W:암컷, M:수컷
+    나이: 0/2 형식으로 저장. 0세2개월
+    체중: 0.0 형식으로 저장
+    입소일: 2021-01-01 형식으로 저장. "2011-09-01T13:20:30+03:00" 형식변환 체크
+    소개영상: 유튜브 링크
+    프로필사진: 이미지파일(유투브 링크 썸네일 추출)
+    '''
+    center = models.CharField(max_length=13, blank=True, default='익명의 챠우')
+    species = models.CharField(max_length=2, choices=SPECIES_CHOICES, default='0')
+    kind = models.CharField(max_length=10, blank=True)
+    sex = models.CharField(max_length=1, blank=True)
+    age=models.CharField(max_length=5, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    enter= models.DateTimeField(default=timezone.now)
+    youtube=models.CharField(max_length=100, blank=True)
+    profile_pic = models.ImageField(upload_to='animal_profile_pics/', blank=True, null=True, default='default_animal.png')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.species}"
