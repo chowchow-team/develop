393f8d8a19d80fdf3c30cf5cfbc4870c6057e7b5
MTW <mtw@MTWui-MacBookPro.local>
2024-08-13 15:50:03 +0900

    llmapp cron.py 수정

--- backend/llmapp/cron.py
+++ backend/llmapp/cron.py
@@ -10,6 +10,8 @@ from django.contrib.auth import get_user_model
 import django
 import logging
 from django.conf import settings
+import random
+
 
 # Add the project root to the Python path
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -46,6 +48,8 @@ def extract_text_from_html(html_content):
     return text.strip()
 
 def llm_post(user):
+    if user.char_num == -1:
+        user.char_num = random.randrange(0,7)
     logger.info(f"Starting llm_post for user: {user.username}")
     # 프롬프트 템플릿 설정
     profile = ProfileSerializer(user)
@@ -72,67 +76,114 @@ def llm_post(user):
         n_threads=8,  # 스레드 수 설정 
     )
     
-    characters = ["쾌활하지만 철학적인 사고를 자주함","음식을 좋아하는 미식가임. 음식의 특징을 잘 파악하며 소탈할 말투로 음식 리뷰글을 씀",]
+    characters = ["쾌활하지만 철학적인 사고를 자주함","음식을 좋아하는 미식가임. 음식의 특징을 잘 파악하며 소탈할 말투로 음식 리뷰글을 씀",
+                  "망상을 자주하는 INFP임", "문학적 감각이 뛰어나고 일기를 짧은 시처럼 씀", "집순이로 밖에 나가기를 싫어함", "재밌는 사행시를 잘 지음",
+                  "아재 개그를 자주힘"]
     output_styles = ["""
     허허,,, 저랑은 좀,, 반대네요,,, 손주 학교 앞에 가면,,, 형이냐구 막;;; 물어보던디;;; ㅎㅎㅎ,,,
     자랑은 아닙니다,, ㅎㅎㅎ 젊게 사는 게 좋지요~~,, 꽃 한 송이 놓구 갑니다~~@>~~~~
     """,
     """
-    ✨더미식 비빔면✨
+    ✨오리젠✨
 
     평점: 8.5/10
 
-    볶은 고추와 육수로 만들어진 매콤한 양념장에 다양한 과일이 들어가서 상큼하고도 달콤하면서도 또 매콤한 밸런스가 잘 어울리는 조합이었소이다. 
-    고기와 함께 먹는 비빔면으로 이 제품을 추천하오. 팔도비빔면에 비해서 면발이 조금 굵은 느낌이었소.
+    닭, 칠면조, 계란의 조합으로 만들어진 사료는 달콤하면서도 단백질 함량이 높아 맛의 밸런스가 뛰어난 조합이었소. 
+    신선하고 단백질이 풍부한 사료가 먹고 싶다면 이 제품을 추천하오. 일반사료에 비해 씹는 맛이 있었소.
     """,
     """
-    
+    뻘하게 갖고 싶은 능력들
+
+    1. 이빨에 치석을 제거하는 능력
+    2. 발톱이 항상 적정 길이로 유지되는 능력
+    3. 털에 묻은 물이 바로 마르는 능력
+    4. 스파이 패밀리의 본드 포저처럼 미래를 예지하는 능력
+    5. 웰레스와 그로밋의 그로밋처럼 계산을 잘하는 능력
+    """,
+    f"""
+    배식소
+
+                        {profile_data['profile']['nickname']}
+    유기견 보호사는 항상 많이 먹으라한다.
+
+    많이 줘야 많이 먹지...
+    """,
+    f"""
+    뚱냥이: {profile_data['profile']['nickname']}
+
+    아침부터 "너 산책 가기 싫어?"라는 말을 들었다.
+
+    진정 나를 잘 이해하는 유기견 보호사와 함께 지내고 있다.
+    """,
+
     """
+    아: 아버지
+    나: 나를 낳으시고
+    바: 바지석삼
+    다: 다 적시셨네.
+    """,
+    """
+    돼지가 떨어지면?
+
+
+
+
+
+
+
+    돈벼락
+    """,
     ]
 
-    # 입력 데이터
-    logger.info(f"Animal profile data: {profile_data}")
-    input_text = f"""종: {profile_data['profile']['species']}
-    이름: {profile_data['profile']['nickname']}
-    성별: {profile_data['profile']['sex']}
-    현재시간: 오후 1시
-    성격: {character}
-    말투 예시: 
-    """
+    # 0에서 6 사이의 랜덤한 정수를 생성
+    num = user.char_num
+    print("!"*30)
+    print(num)
+    if True:
+        # 입력 데이터
+        logger.info(f"Animal profile data: {profile_data}")
+        input_text = f"""종: {profile_data['profile']['species']}
+        이름: {profile_data['profile']['nickname']}
+        성별: {profile_data['profile']['sex']}
+        현재시간: 오후 1시
+        성격: {characters[num]}
+        """
 
-    prompt = f"""
-    {input_text}
+        prompt = f"""
+        {input_text}
 
-    이제, 마치 너가 {input_text}에 설명된 인물이라 생각하고, SNS에 올릴 짧고 일상적인 글을 작성해줘. 이 글은 약 200자 정도로, 쾌활하면서도 철학적인 사고를 반영해야 해. 또한, 아래의 예시와 같은 친근하고 유머러스한 말투를 사용해야 해.
+        이제, 마치 너가 {input_text}에 설명된 인물이라 생각하고, SNS에 올릴 짧고 일상적인 글을 작성해줘. 이 글은 약 200자 정도로, 쾌활하면서도 철학적인 사고를 반영해야 해. 또한, 아래의 예시와 같은 친근하고 유머러스한 말투를 사용해야 해.
 
-    스타일 예시:
-    {output_style}
+        스타일 예시:
+        {output_styles[num]}
 
-    이제 글을 작성해줘.
-    """
+        이제 글을 작성해줘.
+        """
+        print(f"prompt: {prompt}")
 
-    logger.info("Generating post content")
-    # 모델에 프롬프트를 전달하고 응답을 받음
-    response = llm.invoke(prompt)
+        logger.info("Generating post content")
+        # 모델에 프롬프트를 전달하고 응답을 받음
+        response = llm.invoke(prompt)
 
-    logger.info(f"Generated post content for user {user.username}: {response[:100]}...")  # 처음 100자만 로깅
-    post = Post(user=user, content=response.strip(), view_count=0)
-    post.save()
-    logger.info(f"Post saved for user {user.username} with ID: {post.id}")
+        logger.info(f"Generated post content for user {user.username}: {response[:100]}...")  # 처음 100자만 로깅
+        post = Post(user=user, content=response.strip(), view_count=0)
+        post.save()
+        logger.info(f"Post saved for user {user.username} with ID: {post.id}")
 
-    return post
+        return post
 
 User = get_user_model()
 
 def update_animals():
     logger.info("Starting update_animals function")
     logger.info(f"Current working directory: {os.getcwd()}")
-    users = User.objects.filter(is_animal=True)[:5]
+    users = User.objects.filter(is_animal=True)[10:13]
     logger.info(f"Found {len(users)} animal users to update")
-    
     for user in users:
         try:
             post = llm_post(user)
+            print("!"*30)
+            print(user.char_num)
             logger.info(f"Successfully created post for user {user.username}: {post.id}")
         except Exception as e:
             logger.error(f"Error creating post for user {user.username}: {str(e)}")
